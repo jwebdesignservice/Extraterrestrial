ce7300fbb2cfbd8e746952df400518bd9db99061
Jack949 <jackwilson9090@gmail.com>
2026-02-15 21:05:03 +0000

    Remove Leaflet attribution from map

--- src/components/map/WorldMap.tsx
+++ src/components/map/WorldMap.tsx
@@ -410,7 +410,7 @@ export default function WorldMap({ sightings, onMarkerClick }: WorldMapProps) {
     >
       <TileLayer
         url="https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        attribution=""
       />
       
       {/* Heat map background dots - non-clickable */}
